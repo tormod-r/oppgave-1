4730844aba747c1268506bd3d7f434540e96ba06
torroes <torroes@hedmark.org>
2020-04-23 10:14:54 +0200

    u
    tt

---
+++ ethernet/data.php
@@ -0,0 +1,20 @@
+<?php
+include ('connection.php');
+$sql_insert = "INSERT INTO data (temperature, humidity) VALUES ('".$_GET["temperature"]."', '".$_GET["humidity"]."')";
+if(mysqli_query($con,$sql_insert))
+{
+echo "Done";
+mysqli_close($con);
+}
+else
+{
+echo "error is ".mysqli_error($con );
+}
+
+?>
+<html>
+
+<body>
+    <br>
+    <a href="display.php">vis data</a>
+</body></html>
---
+++ ethernet/getData.php
@@ -0,0 +1,14 @@
+<html>
+       
+    <body>
+        <form method="GET" action="data.php">
+            <label>Temperatur:</label>
+            <input type="text" name="temperature" >
+            <br>
+            <label>Humidity:</label>
+            <input type="text" name="humidity" >
+            <br>
+            <button type="submit">send data</button>
+            </form> 
+        </body>
+    </html>
